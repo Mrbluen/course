4e161c47b789d605de119f01b2d71cb1eef80e47
orange <email@e1195643053@qq.com>
2021-11-09 17:50:14 +0800

    改造chapter

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './app.vue'
 import router from './router'
-Vue.config.productionTip = false
+import axios from 'axios'
+
+Vue.prototype.$ajax = axios;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
