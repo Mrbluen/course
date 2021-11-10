46cf3354a706b68fd572e0927bc712b8283ff592
“mrblue” <mrbluen@163.com>
2021-11-10 16:53:44 +0800

    axios拦截器配置

--- admin/src/main.js
+++ admin/src/main.js
@@ -5,6 +5,18 @@ import axios from 'axios'
 
 Vue.prototype.$ajax = axios;
 Vue.config.productionTip = false;
+/**
+* axios拦截器
+* */
+axios.interceptors.request.use(function (config){
+  console.log("请求：",config);
+  return config;
+}, error => {});
+
+axios.interceptors.response.use(function (response){
+  console.log("返回结果：",response);
+  return response;
+}, error => {});
 
 new Vue({
   router,
