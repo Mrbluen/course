dd504947bcee0e7f29b3f71cdf72d80961379221
orange <email@e1195643053@qq.com>
2021-11-17 15:13:45 +0800

    添加最后代码，完结1

--- server/src/main/java/com/course/server/dto/PageDto.java
+++ server/src/main/java/com/course/server/dto/PageDto.java
@@ -3,17 +3,20 @@ package com.course.server.dto;
 import java.util.List;
 
 public class PageDto<T> {
+
     /**
      * 当前页码
-     * */
+     */
     protected int page;
+
     /**
      * 每页条数
-     * */
+     */
     protected int size;
+
     /**
      * 总条数
-     * */
+     */
     protected long total;
 
     protected List<T> list;
@@ -48,5 +51,17 @@ public class PageDto<T> {
 
     public void setList(List<T> list) {
         this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("PageDto{");
+        sb.append("page=").append(page);
+        sb.append(", size=").append(size);
+        sb.append(", total=").append(total);
+        sb.append(", list=").append(list);
+
+        sb.append('}');
+        return sb.toString();
     }
 }
